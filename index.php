9f010ddd9ad850ff4f5544d04ed216bcee5b791d
Matt <matt@zivtech.com>
2017-11-08 13:11:27 -0500

    Test message

--- index.php
+++ index.php
@@ -14,7 +14,6 @@
     die("You suck: " . $conn->connect_error . $eol);
   }
   echo "Goose liver" . $eol;
-  echo "This is kinda bad" . $eol;
   echo "What, me worry?" . $eol;
   $res = mysqli_query($conn, 'SELECT * FROM hello');
   while($row = mysqli_fetch_assoc($res)) {
