f084549aa0262e5387fb5e2efdcb8ddeaba98e38
Matt <matt@zivtech.com>
2017-11-09 12:02:17 -0500

    Test Message

--- index.php
+++ index.php
@@ -2,9 +2,10 @@
 
   $servername = "localhost";
   $username = "root";
-  $password = "root";
+  $password = "";
 
   $eol = "<br/>" . PHP_EOL;
+  echo "Goose liver" . $eol;
 
   // Create connection
   $conn = new mysqli($servername, $username, $password, 'lampdb');
@@ -13,10 +14,7 @@
   if ($conn->connect_error) {
     die("You suck: " . $conn->connect_error . $eol);
   }
-  echo "Goose liver" . $eol;
-  echo "What, me worry?" . $eol;
-  echo "I'm bad at this" . $eol;
-  echo "You should feel bad about it" . $eol;
+
   $res = mysqli_query($conn, 'SELECT * FROM hello');
   while($row = mysqli_fetch_assoc($res)) {
     echo "david the gnome " . $row['planet'] . $eol;
